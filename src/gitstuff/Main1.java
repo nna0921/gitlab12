080eff05c94f3b316e6a586a87668b2c232142a0
nna0921 <f223658@cfd.nu.edu.pk>
2024-11-14 23:32:35 +0500

    line removed

--- src/gitstuff/Main1.java
+++ src/gitstuff/Main1.java
@@ -3,7 +3,7 @@ package gitstuff;
 public class Main1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		greetUser();
+		
 	}
 
 	public static void greetUser() {
