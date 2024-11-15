63cf8d3c720e23213530971680f1023c2e213013
nna0921 <f223658@cfd.nu.edu.pk>
2024-11-15 10:45:34 +0500

    fixed critical bug in main class

---
+++ src/gitstuff/UserProfile.java
@@ -0,0 +1,17 @@
+package gitstuff;
+
+public class UserProfile {
+	private String name;
+	private String email;
+
+	public UserProfile(String name, String email) {
+	this.name = name;
+	this.email = email;
+	}
+
+	public void displayProfile() {
+	System.out.println("Name: " + name);
+	System.out.println("Email: " + email);
+	}
+	
+}
